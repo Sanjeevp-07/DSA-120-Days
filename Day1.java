92b2c359c4999ae50fcfee067b81ff261fbbfa30
Sanjeevp-07 <sanjeev1803t@gmail.com>
2026-09-13 16:11:50 +0530

    Day1 complete

--- Day1.java
+++ Day1.java
@@ -13,8 +13,6 @@ public class Day1{
     }
 } */
 
-
-
 /*Q2. Print numbers from N to 1. 
 import java.util.Scanner;
 
@@ -31,8 +29,6 @@ public class Day1{
 
  */
 
-
-
 /*Q3. Print all even numbers from 1 to N.
  import java.util.Scanner;
 
@@ -46,9 +42,6 @@ public class Day1{
         }
     }
 } */
-
-
-
 
 /* 
 Q4. Find the sum of numbers from 1 to N.
@@ -67,10 +60,6 @@ public class Day1{
         System.out.println(sum);
     }
 } */
-
-
-
-
 
 //Q5. Given N, calculate N!.
 /* import java.util.Scanner; 
@@ -91,10 +80,6 @@ public class Day1{
         System.out.println(result);
     }
 } */
-
-
-
-
 
 /* Q6. Count the number of digits in N.
 import java.util.Scanner;
@@ -123,10 +108,7 @@ public class Day1{
     }
 } */
 
-
-
-
-//Q7. Reverse a number.
+/* Q7. Reverse a number.
 import java.util.Scanner;
 
 public class Day1{
@@ -149,4 +131,140 @@ public class Day1{
         int result = Reverse(n);
         System.out.println(result);
     }
-}
+} */
+
+/* Q8. Find the sum of digits.
+import java.util.Scanner;
+public class Day1{
+    static int sumDigit(int n){
+        int sum = 0;
+        int temp = 0;
+
+        while(n != 0){
+            temp = n % 10;
+            sum += temp;
+
+            n /= 10;
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int result = sumDigit(n);
+        System.out.println(result);
+    }
+} */
+
+/* Q9. Check whether a number is a palindrome.
+import java.util.Scanner;
+
+public class Day1{
+
+    static boolean palindrome(int n){
+        int temp = 0;
+        int rev = 0;
+        int original = n;
+
+        while(n != 0){
+            temp = n % 10;
+            rev = (rev * 10) + temp;
+
+            n /= 10;
+        }
+        if(original == rev){
+            return true;
+        }
+
+        return false;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        boolean result = palindrome(n);
+        System.out.println(result);
+    }
+} */
+
+/* Q10.Print:
+
+*
+**
+***
+****
+*****
+
+public class Day1{
+    public static void main(String[] args){
+        for(int i = 1; i <= 5; i++ ){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} */
+
+/* Q11.Print:
+
+*****
+****
+***
+**
+*
+public class Day1{
+    public static void main(String[] args){
+        for(int i = 5; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} */
+
+/* Q12.Print:
+
+1
+12
+123
+1234
+12345
+
+public class Day1{
+    public static void main(String[] args){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}  */
+
+
+
+
+/* Q14.Print:
+*****
+*   *
+*   *
+*   *
+*****
+
+public class Day1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+} */
