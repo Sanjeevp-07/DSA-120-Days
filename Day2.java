69f55317fa2331f10e9b033f62ca6649e965538b
Sanjeevp-07 <sanjeev1803t@gmail.com>
2026-09-14 13:47:15 +0530

    11 Basic Question complete

--- Day2.java
+++ Day2.java
@@ -19,9 +19,6 @@ public class Day2{
     }
 } */
 
-
-
-
 /* Q2:Find the sum of all elements
 import java.util.Scanner;
 public class Day2{
@@ -40,10 +37,6 @@ public class Day2{
         
     }
 } */
-
-
-
-
 
 /* Q3:Find the maximum element.
 import java.util.Scanner;
@@ -68,9 +61,6 @@ public class Day2{
         System.out.print(max + " is a Largest Element");
     }
 } */
-
-
-
 
 /* Q4:Find the minimum element.
 import java.util.Scanner;
@@ -97,13 +87,6 @@ public class Day2{
     }
 } */
 
-
-
-
-
-
-
-
 /* Q5:Count how many elements are even.
 import java.util.Scanner;
 
@@ -128,14 +111,6 @@ public class Day2{
         System.out.print(count);
     }
 } */
-
-
-
-
-
-
-
-
 
 /* Q6:Count how many elements are greater than X.
 import java.util.Scanner;
@@ -163,19 +138,12 @@ public class Day2{
     }
 } */
 
-
-
-
-
-
-
-
 /* Q7:Given an array and target X, print the index of the first occurrence.
 If not found:-1
 Example:
 Array: 10 20 30 20 40
 X = 20
-Output:1 */
+Output:1
 
 import java.util.Scanner;
 
@@ -198,4 +166,103 @@ public class Day2{
             }
         }
     }
-}
+} */
+
+/* Q9:Check whether the array is sorted in increasing order.
+Example:
+1 2 3 4 5 → true
+1 3 2 4 5 → false
+
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean isSorted = true;
+
+        for(int i = 0; i < arr.length - 1; i ++){
+            if(arr[i] > arr[i + 1]){
+                isSorted = false;
+                break;
+            }
+        }
+        System.out.print(isSorted);
+    }
+} */
+
+
+
+
+/* Q10:Reverse the array in-place. Don't create another array.
+Example:
+Input:1 2 3 4 5
+Output:5 4 3 2 1
+import java.util.Scanner;
+
+public class Day2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right){
+            int temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+
+
+            left++;
+            right--;
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+} */
+
+
+
+
+
+//Q11:Find the second largest element without sorting.
+import java.util.Scanner;
+public class Day2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < arr.length; i++){
+            if(largest < arr[i]){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+        }
+        System.out.print(secondLargest + " is the Second largest element in the array");
+    }
+} 
