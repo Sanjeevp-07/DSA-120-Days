30ad73e6979046d2c68416ace2ed2943a6298eef
Sanjeevp-07 <sanjeev1803t@gmail.com>
2026-09-15 16:50:37 +0530

    Day 3 Complete

--- Day3.java
+++ Day3.java
@@ -136,7 +136,7 @@ public class Day3{
 
 
 
-//Q6:bubbleSort
+/* //Q6:bubbleSort
 import java.util.Arrays;
 public class Day3{
 
@@ -161,4 +161,122 @@ public class Day3{
         System.out.print(Arrays.toString(sortedArray));
     
     }
+} */
+
+
+
+
+
+
+/* //Q7 — Selection Sort
+import java.util.Arrays;
+public class Day3{
+    static int[] selectionSort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+        int[] arr = {64, 25, 12, 22, 11};
+        int[] sortedArray = selectionSort(arr);
+
+        System.out.print(Arrays.toString(sortedArray));
+    }
+} */
+
+
+
+
+/* //Q8:Find the frequency of 2 using a HashMap. Then find its frequency without a HashMap. Compare the two approaches.
+//Given: [4, 2, 7, 2, 9, 2]
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Day3{
+    public static void main(String[] args){
+        //HashMap <Integer, Integer> freq = new HashMap<>();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int x : arr){
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+        System.out.print(freq.get(2));
+
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 2){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+} */
+
+
+
+
+
+
+//Q9:Given a sorted array: [2, 5, 8, 12, 16, 23, 38, 56, 72]
+//Find 23 using binary search. Write down the values of:
+//left, right, mid at every iteration.
+
+public class Day3{
+
+    static int findElement(int[] arr, int target){
+        int left = 0; 
+        int right = arr.length - 1; 
+
+        
+        
+        for(int i = 0; i < arr.length; i++){
+            int mid = left + (right - left) / 2;
+            System.out.println(
+                    "left = " + left +
+                    ", right = " + right +
+                    ", mid = " + mid
+                );
+
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] < target){
+                left = mid + 1;
+            }else if(arr[mid] > target){
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+
+
+    public static void main(String[] args){
+
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72};
+        
+        int result = findElement(arr, 23);
+
+        System.out.println(result);
+    }
 }
+
+
